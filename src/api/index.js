7519357cffb4daf8697a084d36e75b75e3c15b66
Devesh Kumar <deveshkumar21101@gmail.com>
2022-08-06 17:51:52 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -6,23 +6,6 @@ const process = require("node:process");
 
 const port = process.env.PORT;
 
-if (cluster.isPrimary) {
-  //console.log(`Primary ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    //console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  http.createServer(app).listen(port, () => {
-    //console.log("Server listening on port " + process.env.PORT);
-  });
-
-  //console.log(`Worker ${process.pid} started`);
-}
+http.createServer(app).listen(port, () => {
+  //console.log("Server listening on port " + process.env.PORT);
+});
